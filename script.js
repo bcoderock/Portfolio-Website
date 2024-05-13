f98dd0abc97cde6b139ffff237cc25fec074da25
Bernice Kipkeny <bcoderock@gmail.com>
2024-05-13 18:36:12 -0400

    more styling naviagation

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggle_menu(){
+    const menu=document.querySelector(".menu-links")
+    const icon=document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toogle("open")
+}
