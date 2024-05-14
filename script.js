fe3847647fcec8ea6064ae67da04f2fcc7a6ffd3
Bernice Kipkeny <bcoderock@gmail.com>
2024-05-13 21:14:11 -0400

    more css and change in pictures

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function toggle_menu(){
+function toggleMenu(){
     const menu=document.querySelector(".menu-links")
     const icon=document.querySelector(".hamburger-icon");
     menu.classList.toggle("open")
