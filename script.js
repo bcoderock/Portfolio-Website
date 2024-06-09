28045ba441e4f1d73f27f18c39fd4286e8bdc823
Bernice Kipkeny <bcoderock@gmail.com>
2024-06-09 16:24:03 -0400

    restart

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function toggleMenu(){
-    const menu=document.querySelector(".menu-links")
-    const icon=document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open")
-    icon.classList.toogle("open")
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function toggleMenu(){
+//     const menu=document.querySelector(".menu-links")
+//     const icon=document.querySelector(".hamburger-icon");
+//     menu.classList.toggle("open")
+//     icon.classList.toogle("open")
+// }
